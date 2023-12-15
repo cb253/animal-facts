c79c1a495476981e6a51f090a4031a22ee12136f
Christian Belarde <cjbelarde253@gmail.com>
2023-12-14 16:59:22 -0800

    spelling fixes

--- source.ps1
+++ source.ps1
@@ -5,25 +5,25 @@ Clear-Host
 Write-Host "Welcome to Animal Facts!"
 $factGiven = $false
 DO {
-     if (!$factGiven) {
+    if (!$factGiven) {
         $userChoice = Read-Host "`nGenerate [d] Dog or [c] Cat fact?"
         $factGiven = $true
-        }        
+    }        
     if ($userChoice.ToLower() -eq "d") {
         $dogFact = Invoke-RestMethod -Uri $dogFactUrl -Method Get 
-            Clear-Host
-            Write-Host $dogFact.data.attributes.body
-            $factGiven = $false
-        } elseif ($userChoice.ToLower() -eq "c") {
-            $catFact = Invoke-RestMethod -Uri $catFactUrl -Method Get
-            Clear-Host
-            Write-Host $catFact.fact
-            $factGiven = $false
-        } elseif ($userChoice.ToLower() -eq "exit") {
-            exit
-        } else {
-            Clear-Host
-            Write-Host "Please select a valid option, or type exit quit"
-            $factGiven = $false
-        }
+        Clear-Host
+        Write-Host $dogFact.data.attributes.body
+        $factGiven = $false
+    } elseif ($userChoice.ToLower() -eq "c") {
+        $catFact = Invoke-RestMethod -Uri $catFactUrl -Method Get
+        Clear-Host
+        Write-Host $catFact.fact
+        $factGiven = $false
+    } elseif ($userChoice.ToLower() -eq "exit") {
+        exit
+    } else {
+        Clear-Host
+        Write-Host "Please select a valid option, or type exit to quit"
+        $factGiven = $false
+    }
 } Until ($userChoice.ToLower() -eq "exit" -or $userChoice.ToLower() -eq "n")
